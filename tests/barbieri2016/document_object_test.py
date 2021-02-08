21866a7d4d113afa8d6583cf76d2c9d5e770696c
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2021-02-08 22:03:58 +0900

    update

--- tests/barbieri2016/document_object_test.py
+++ tests/barbieri2016/document_object_test.py
@@ -1,18 +1,35 @@
 import pytest
-from bs4 import BeautifulSoup
 from felp.barbieri2016.document_object import DocumentObjectFeature
 from felp.common.testing import FelpTestCase
 
 
 class TestDocumentObjectFeature(FelpTestCase):
+    @pytest.mark.skip()
     @pytest.mark.parametrize(
-        "html_file, expected",
-        (("lp01.html", 14), ("lp02.html", 90), ("lp03.html", 88)),
+        "soup, expected", (("lp01", 14), ("lp02", 90), ("lp03", 88)), indirect=["soup"]
     )
-    def test_get_num_of_internal_links(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_internal_links(self, soup, expected):
+        extractor = DocumentObjectFeature(
+            lp_url="https://felp_dummy_internal.com", soup=soup
+        )
+        internal_links = extractor.get_internal_links()
+        raise NotImplementedError
+
+    @pytest.mark.skip()
+    @pytest.mark.parametrize(
+        "soup, expected", (("lp01", 14), ("lp02", 90), ("lp03", 88)), indirect=["soup"]
+    )
+    def test_get_external_links(self, soup, expected):
+        extractor = DocumentObjectFeature(
+            lp_url="https://felp_dummy_internal.com", soup=soup
+        )
+        external_links = extractor.get_external_links()
+        raise NotImplementedError
+
+    @pytest.mark.parametrize(
+        "soup, expected", (("lp01", 17), ("lp02", 72), ("lp03", 65)), indirect=["soup"]
+    )
+    def test_get_num_of_internal_links(self, soup, expected):
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -20,13 +37,10 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert num_of_internal_links == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
-        (("lp01.html", 13), ("lp02.html", 45), ("lp03.html", 46)),
+        "soup, expected", (("lp01", 9), ("lp02", 35), ("lp03", 31)), indirect=["soup"]
     )
-    def test_get_num_of_external_links(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_num_of_external_links(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -35,17 +49,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert num_of_external_links == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 27),
-            ("lp02.html", 135),
-            ("lp03.html", 134),
+            ("lp01", 26),
+            ("lp02", 107),
+            ("lp03", 96),
         ),
+        indirect=["soup"],
     )
-    def test_num_of_links(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_num_of_links(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -54,17 +67,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert num_of_links == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected_internal, expected_external",
+        "soup, expected_internal, expected_external",
         (
-            ("lp01.html", 14, 13),
-            ("lp02.html", 90, 45),
-            ("lp03.html", 88, 46),
+            ("lp01", 17, 9),
+            ("lp02", 72, 35),
+            ("lp03", 65, 31),
         ),
+        indirect=["soup"],
     )
-    def test_get_links(self, html_file, expected_internal, expected_external):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_links(self, soup, expected_internal, expected_external):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -75,40 +87,38 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert len(links.external) == expected_external
 
     @pytest.mark.parametrize(
-        "html_file, expected_file",
+        "soup, expected_main_text_num",
         (
-            ("lp01.html", "lp01_main_text.txt"),
-            ("lp02.html", "lp02_main_text.txt"),
-            ("lp03.html", "lp03_main_text.txt"),
+            ("lp01", "lp01"),
+            ("lp02", "lp02"),
+            ("lp03", "lp03"),
         ),
+        indirect=["soup"],
     )
-    def test_get_main_text(self, html_file, expected_file):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_main_text(self, soup, expected_main_text_num):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
         main_text = extractor.get_main_text()
 
-        expected_path = self.FIXTURES_ROOT / expected_file
+        expected_path = self.FIXTURES_ROOT / expected_main_text_num / "main_text.txt"
         with expected_path.open("r") as rf:
             expected_text = rf.read()
 
         assert main_text == expected_text
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 0.93),
-            ("lp02.html", 0.50),
-            ("lp03.html", 0.52),
+            ("lp01", 0.53),
+            ("lp02", 0.49),
+            ("lp03", 0.48),
         ),
+        indirect=["soup"],
     )
-    def test_get_number_of_external_internal_link_ratio(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_number_of_external_internal_link_ratio(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -119,17 +129,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert round(number_of_external_internal_link_ratio, 2) == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 0.52),
-            ("lp02.html", 0.67),
-            ("lp03.html", 0.66),
+            ("lp01", 0.65),
+            ("lp02", 0.67),
+            ("lp03", 0.68),
         ),
+        indirect=["soup"],
     )
-    def test_get_number_of_internal_total_links_ratio(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_number_of_internal_total_links_ratio(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -140,17 +149,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert round(number_of_internal_total_links_ratio, 2) == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 0.48),
-            ("lp02.html", 0.33),
-            ("lp03.html", 0.34),
+            ("lp01", 0.35),
+            ("lp02", 0.33),
+            ("lp03", 0.32),
         ),
+        indirect=["soup"],
     )
-    def test_number_of_external_total_links_ratio(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_number_of_external_total_links_ratio(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -161,17 +169,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert round(number_of_external_total_links_ratio, 2) == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 0.13),
-            ("lp02.html", 0.27),
-            ("lp03.html", 0.28),
+            ("lp01", 0.38),
+            ("lp02", 0.3),
+            ("lp03", 0.29),
         ),
+        indirect=["soup"],
     )
-    def test_get_text_size_internal_links_ratio(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_text_size_internal_links_ratio(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -180,17 +187,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert round(text_size_internal_links_ratio, 2) == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 0.09),
-            ("lp02.html", 0.08),
-            ("lp03.html", 0.08),
+            ("lp01", 0.08),
+            ("lp02", 0.1),
+            ("lp03", 0.09),
         ),
+        indirect=["soup"],
     )
-    def test_get_text_size_external_links_ratio(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_text_size_external_links_ratio(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -199,17 +205,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert round(text_size_external_links_ratio, 2) == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 0.22),
-            ("lp02.html", 0.35),
-            ("lp03.html", 0.36),
+            ("lp01", 0.46),
+            ("lp02", 0.41),
+            ("lp03", 0.38),
         ),
+        indirect=["soup"],
     )
-    def test_get_text_size_links_ratio(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_text_size_links_ratio(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -218,17 +223,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert round(text_size_links_ratio, 2) == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 0.12),
-            ("lp02.html", 0.26),
-            ("lp03.html", 0.27),
+            ("lp01", 0.36),
+            ("lp02", 0.29),
+            ("lp03", 0.28),
         ),
+        indirect=["soup"],
     )
-    def test_get_main_text_size_internal_links_ratio(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_main_text_size_internal_links_ratio(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
@@ -239,17 +243,16 @@ class TestDocumentObjectFeature(FelpTestCase):
         assert round(main_text_size_internal_links, 2) == expected
 
     @pytest.mark.parametrize(
-        "html_file, expected",
+        "soup, expected",
         (
-            ("lp01.html", 0.12),
-            ("lp02.html", 0.26),
-            ("lp03.html", 0.27),
+            ("lp01", 0.36),
+            ("lp02", 0.29),
+            ("lp03", 0.28),
         ),
+        indirect=["soup"],
     )
-    def test_get_main_text_size_external_links_ratio(self, html_file, expected):
-        html_path = self.FIXTURES_ROOT / html_file
-        with html_path.open("r") as rf:
-            soup = BeautifulSoup(rf, "lxml")
+    def test_get_main_text_size_external_links_ratio(self, soup, expected):
+
         extractor = DocumentObjectFeature(
             lp_url="https://felp_dummy_internal.com", soup=soup
         )
